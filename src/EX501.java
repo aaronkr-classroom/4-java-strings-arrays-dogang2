07ecfe265fbf99a74cb777924c881b2a8e46f2cd
20211814김동연 <161808977+dogang2@users.noreply.github.com>
2024-03-26 15:58:00 +0900

    5장 끝

---
+++ src/EX501.java
@@ -0,0 +1,17 @@
+
+public class EX501 {
+	// My Method
+	public static void hello(String name) {
+		System.out.println("Hello, " + name);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		hello("Aaron");
+		hello("Tom");
+		hello("Stven");
+		hello("Pocky");
+		hello("Socky");
+		
+	}
+
+}
